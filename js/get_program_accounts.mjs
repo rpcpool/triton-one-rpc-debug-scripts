0f5534a03413b136ea555a49e9d09f9e384749d9
Brian Long <brian.long@fmadata.com>
2023-04-10 06:55:15 -0500

    Cleanup scripts

--- js/get_program_accounts.mjs
+++ js/get_program_accounts.mjs
@@ -1,4 +1,4 @@
-// This script will run a getProgramAccounts query on the Serum V3 program ID. 
+// This script will run a getProgramAccounts query on the Stake... program ID. 
 // Output to the console will show the number of records returned and the 
 // elapsed time in milliseconds. On error, the script will exit and dump 
 // error details to the console.
@@ -17,38 +17,43 @@ const connection = new web3.Connection(process.env.RPC_URL, 'confirmed');
 
 // Setup getProgramAccounts call
 //
-// Serum
-// const program_id = '9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'; // Program
-// const gpa_owner_id = 'FnJZCfHc6LuZKCWMJGsHmpmqPeVVpxiJR3upiFp8TWsG'; // Filter
-// const data_size = 3228;
-// const offset = 45;
-//
-// Tokenkeg
-const program_id = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'; // Program
-const gpa_owner_id = 'CwyQtt6xGptR7PaxrrksgqBSRCZ3Zb2GjUYjKD9jH3tf'; // Filter
-const data_size = 165;
-const offset = 32;
+// Marinade
+const program_id = 'Stake11111111111111111111111111111111111111';
+const bytes = '4bZ6o3eUUNXhKuqjdCnCoPAoLgWiuLYixKaxoa8PpiKk';
+const offset = 12;
+const gpa_filters = [
+  // { dataSize: data_size },
+  { memcmp: { offset: offset, bytes: bytes } }
+];
 
 console.log('Checking:',process.env.RPC_URL,"\n");
 
+// Run getProgramAccounts
 try {
   let start_time = new Date();
   const program_key = new web3.PublicKey(program_id);
 
   const accounts = await connection.getProgramAccounts(
     program_key,
-    {
-      filters: [
-        { dataSize: data_size },
-        { memcmp: { offset: offset, bytes: gpa_owner_id } }
-      ]
-    }
+    { filters: gpa_filters }
   );
 
   let elapsed_time = new Date() - start_time;
-  console.log(accounts.length, 'accounts in', elapsed_time, "milliseconds\n");
-  console.log(JSON.stringify(accounts));
-  console.log(accounts.length, 'accounts in', elapsed_time, "milliseconds\n");
+  console.log(new Date().toISOString(), accounts.length, 'accounts in', elapsed_time, "milliseconds\n");
 } catch (e) {
   console.log('\n', e, '\n');
 }
+
+// Appendix:
+//
+// OpenBook
+// const program_id = 'srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'; // Program
+// const bytes = '5jWUncPNBMZJ3sTHKmMLszypVkoRK6bfEQMQUHweeQnh'; // Filter
+// const data_size = 3228;
+// const offset = 45;
+// # bytes_list = ["DxFJrKS2demp8TmN2c4Gp4MCQgFBvtK8SvdeeWKyCScE", "2tTPiyXKifAtwLasZwkpdSKmJQvG6LuQ7k8cV3TC5HxM", "3KiMpRp7kH1WBkVCTQ1SMpeFp9HSvfzX7V5k5JkSpqMg",'9ycmYnQmDRhrUTWLspW1Q4Yn8EDtYn3wCUc9Q72YVrmb','8BySRezwLPxgo5r4zCnfmaxTt16ufCPD7FP475k6K4TT','Cyfs4gas18c3gxHF3Hs2vo7HBKWyi6kjeXXpS93qWKSK','B9FpdsWUqrhPmBNiDGDYRUPV77dFocf32jMJa4G2PwwL','4fEuQEAeySDjDLrzVTr2Mp8GTaJZj4z8f2Zt6r4PSQof']
+// Tokenkeg
+// const program_id = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'; // Program
+// const bytes = 'CwyQtt6xGptR7PaxrrksgqBSRCZ3Zb2GjUYjKD9jH3tf'; // Filter
+// const offset = 32;
+// const data_size = 165;
--- js/get_program_accounts_compare.mjs
+++ js/get_program_accounts_compare.mjs
@@ -2,44 +2,37 @@
 // Output to the console will show the difference between the results of the 
 // RPC_URL and RPC_URL_COMPARE endpoints.
 //
+// Example Use:
 // Provide RPC_URL & RPC_URL_COMPARE in your .env file.
 //
-// Example Use:
 // node get_program_accounts_compare.mjs
 
 import * as web3 from '@solana/web3.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// Setup getProgramAccounts call
-//
-// OpenBook
-// const program_id = 'srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'; // Program
-// const bytes = '5jWUncPNBMZJ3sTHKmMLszypVkoRK6bfEQMQUHweeQnh'; // Filter
-// const data_size = 3228;
-// const offset = 45;
-// # bytes_list = ["DxFJrKS2demp8TmN2c4Gp4MCQgFBvtK8SvdeeWKyCScE", "2tTPiyXKifAtwLasZwkpdSKmJQvG6LuQ7k8cV3TC5HxM", "3KiMpRp7kH1WBkVCTQ1SMpeFp9HSvfzX7V5k5JkSpqMg",'9ycmYnQmDRhrUTWLspW1Q4Yn8EDtYn3wCUc9Q72YVrmb','8BySRezwLPxgo5r4zCnfmaxTt16ufCPD7FP475k6K4TT','Cyfs4gas18c3gxHF3Hs2vo7HBKWyi6kjeXXpS93qWKSK','B9FpdsWUqrhPmBNiDGDYRUPV77dFocf32jMJa4G2PwwL','4fEuQEAeySDjDLrzVTr2Mp8GTaJZj4z8f2Zt6r4PSQof']
-// Tokenkeg
-// const program_id = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'; // Program
-// const bytes = 'CwyQtt6xGptR7PaxrrksgqBSRCZ3Zb2GjUYjKD9jH3tf'; // Filter
-// const offset = 32;
-// const data_size = 165;
+// Setup getProgramAccounts call & gpa_filters.
 //
 // Marinade
 const program_id = 'Stake11111111111111111111111111111111111111';
 const bytes = '4bZ6o3eUUNXhKuqjdCnCoPAoLgWiuLYixKaxoa8PpiKk';
 const offset = 12;
+const gpa_filters = [
+  // { dataSize: data_size },
+  { memcmp: { offset: offset, bytes: bytes } }
+];
 
 const endpoints = {
   'RPC': process.env.RPC_URL,
   'Steamboat': process.env.RPC_URL_COMPARE
 }
 
-// Loop through endpoints and run getProgramAccounts
+// Get pubkeys from getProgramAccounts for each endpoint.
 async function getPubkeys () {
   let pubkeys = {}
   const keys = Object.keys(endpoints);
 
+  // Loop through endpoints and run getProgramAccounts
   const fetchAccounts = keys.map(async (key) => {
     let endpoint = endpoints[key];
     pubkeys[key] = [];
@@ -51,15 +44,9 @@ async function getPubkeys () {
       let start_time = new Date();
       let program_key = new web3.PublicKey(program_id);
 
-      // NOTE: You may need to add/remove filters below depending on your params.
       let accounts = await connection.getProgramAccounts(
         program_key,
-        {
-          filters: [
-            // { dataSize: data_size },
-            { memcmp: { offset: offset, bytes: bytes } }
-          ]
-        }
+        { filters: gpa_filters }
       );
 
       // Add the pubkeys to the pubkeys array
@@ -78,13 +65,13 @@ async function getPubkeys () {
   // Wait for all the promises to resolve
   await Promise.all(fetchAccounts);
 
-  // wait for the results of getProgramAccounts and return the final value of pubkeys
+  // Return the final value of pubkeys
   return pubkeys;
 }
 
 // show the contents of an array that are not in another array
 function diffArray(arr1, arr2) {
-  var newArr = [];
+  let newArr = [];
   // Same, same; but different.
   for (let i = 0; i < arr1.length; i++) {
     if (arr2.indexOf(arr1[i]) === -1) {
@@ -102,3 +89,18 @@ getPubkeys()
   .catch(error => {
     console.error('Error:', error);
   });
+
+// Appendix
+// 
+// OpenBook
+// const program_id = 'srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'; // Program
+// const bytes = '5jWUncPNBMZJ3sTHKmMLszypVkoRK6bfEQMQUHweeQnh'; // Filter
+// const data_size = 3228;
+// const offset = 45;
+// # bytes_list = ["DxFJrKS2demp8TmN2c4Gp4MCQgFBvtK8SvdeeWKyCScE", "2tTPiyXKifAtwLasZwkpdSKmJQvG6LuQ7k8cV3TC5HxM", "3KiMpRp7kH1WBkVCTQ1SMpeFp9HSvfzX7V5k5JkSpqMg",'9ycmYnQmDRhrUTWLspW1Q4Yn8EDtYn3wCUc9Q72YVrmb','8BySRezwLPxgo5r4zCnfmaxTt16ufCPD7FP475k6K4TT','Cyfs4gas18c3gxHF3Hs2vo7HBKWyi6kjeXXpS93qWKSK','B9FpdsWUqrhPmBNiDGDYRUPV77dFocf32jMJa4G2PwwL','4fEuQEAeySDjDLrzVTr2Mp8GTaJZj4z8f2Zt6r4PSQof']
+// Tokenkeg
+// const program_id = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'; // Program
+// const bytes = 'CwyQtt6xGptR7PaxrrksgqBSRCZ3Zb2GjUYjKD9jH3tf'; // Filter
+// const offset = 32;
+// const data_size = 165;
+//
