4b78a4a08fb428a378349d99c4e547d25d9dc3c2
Brian Long <bl@blocklogic.net>
2023-12-03 18:10:02 +0000

    Show counts for vote state hashes

--- js/get_votes_for_block.mjs
+++ js/get_votes_for_block.mjs
@@ -21,6 +21,8 @@ const connection = new web3.Connection(
   process.env.RPC_URL, 'confirmed'
 );
 
+const account_state_votes = {};
+
 const block = await connection._rpcRequest(
   'getBlock',
   connection._buildArgsAtLeastConfirmed(
@@ -45,9 +47,25 @@ block.result.transactions.forEach((tx) => {
   // Loop to the next tx unless accountKeys includes 'Vote111111111111111111111111111111111111111'
   if(!tx.transaction.message.accountKeys.find((key) => key.pubkey == "Vote111111111111111111111111111111111111111")) return;
 
-  console.log(`${JSON.stringify(tx)}\n`);
-  console.log(`${JSON.stringify(tx.transaction.message.instructions[0].parsed)}\n`);
+  // console.log(`${JSON.stringify(tx)}\n`);
+  // console.log(`${JSON.stringify(tx.transaction.message.instructions[0].parsed)}\n`);
+  const lockout_length = tx.transaction.message.instructions[0].parsed.info.voteStateUpdate.lockouts.length;
+  const vote_slot = tx.transaction.message.instructions[0].parsed.info.voteStateUpdate.lockouts[lockout_length-1].slot;
+  const vote_state_hash = tx.transaction.message.instructions[0].parsed.info.voteStateUpdate.hash;
+  if(!account_state_votes[vote_state_hash]) {
+    account_state_votes[vote_state_hash] = 0;
+  }
+  account_state_votes[vote_state_hash] = account_state_votes[vote_state_hash] + 1;
 
-  // TODO: Parse the instruction data
+  const output = [
+    tx.transaction.message.instructions[0].parsed.info.voteStateUpdate.root,
+    vote_slot,
+    vote_slot - slot_number,
+    vote_state_hash,
+    tx.transaction.message.instructions[0].parsed.info.voteAuthority,
+    tx.transaction.signatures[0]
+  ]
+  console.log(`${JSON.stringify(output)}`);
 
 });
+console.log(account_state_votes);
