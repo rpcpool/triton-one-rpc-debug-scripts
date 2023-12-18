b2218ade0a626b169a2b964cbc9485731e31f533
Brian Long <bl@blocklogic.net>
2023-12-18 14:49:15 +0000

    Adds a script for epoch info.

---
+++ js/get_epoch_info.mjs
@@ -0,0 +1,23 @@
+// This script will run a getProgramAccounts query on the Serum V3 program ID. 
+// Output to the console will show the number of records returned and the 
+// elapsed time in milliseconds. On error, the script will exit and dump 
+// error details to the console.
+//
+// Provide RPC_URL in your .env file to check your connection.
+//
+// Example Use:
+// node get_epoch_info.mjs
+
+import * as web3 from '@solana/web3.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Set up web3 client
+const connection = new web3.Connection(
+  process.env.RPC_URL, 'finalized'
+);
+
+// Get the latest finalized slot
+const epoch_info = await connection.getEpochInfo(); // 'finalized'
+console.log(epoch_info);
+
