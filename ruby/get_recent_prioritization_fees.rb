b0d56f449f64b690b3dce76057a53122cfe32e71
Brian Long <bl@blocklogic.net>
2023-12-18 13:12:35 +0000

    update notes

--- ruby/get_recent_prioritization_fees.rb
+++ ruby/get_recent_prioritization_fees.rb
@@ -14,7 +14,7 @@ crank_grpf = `curl https://api.mainnet-beta.solana.com -X POST -H "Content-Type:
 # crank_grpf = `curl https://api.mainnet-beta.solana.com -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getRecentPrioritizationFees", "params": [["5yixRcKtcs5BZ1K2FsLFwmES1MyA92d6efvijjVevQCw"]]}'`
 
 # https://solscan.io/tx/4ELeyksZeQuyAv7jWLhDJ8hmWApVoijTND6biPfciAuzaRdHnbZdnaY1H2M8XycDqzzvCf3KTU4YpcUhWmtrbnDw
-# crank_grpf = `curl https://lb-nyc1.rpcpool.com/20dc90af2aed522cdff09bab309e -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getRecentPrioritizationFees", "params": [["HmiHHzq4Fym9e1D4qzLS6LDDM3tNsCTBPDWHTLZ763jY", "5Di65JsuLU7n8RLZBPhWwHyxVTHM1feLXZnX6VjGpG7S", "8BnEgHoWFysVcuFFX7QztDmzuH8r5ZFvyP3sYwn1XTh6", "8CvwxZ9Db6XbLD46NZwwmVDZZRDy7eydFcAGkXKh9axa", "Bzmr3xA8sWsHogARhFvJALR9o58xXk4jUnPZ4ARhez8w"]]}'`
+# crank_grpf = `curl https://api.mainnet-beta.solana.com -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getRecentPrioritizationFees", "params": [["HmiHHzq4Fym9e1D4qzLS6LDDM3tNsCTBPDWHTLZ763jY", "5Di65JsuLU7n8RLZBPhWwHyxVTHM1feLXZnX6VjGpG7S", "8BnEgHoWFysVcuFFX7QztDmzuH8r5ZFvyP3sYwn1XTh6", "8CvwxZ9Db6XbLD46NZwwmVDZZRDy7eydFcAGkXKh9axa", "Bzmr3xA8sWsHogARhFvJALR9o58xXk4jUnPZ4ARhez8w"]]}'`
 
 # SOL/USDC
 # https://solscan.io/tx/5eTR89FQ74BkvP5ReoHXQiQGwctFXbRuajTYAYZHZHzststeJ9kBYtra44gvFT4DeCobxBKhrUewcegQZJecqdDh
@@ -22,10 +22,10 @@ crank_grpf = `curl https://api.mainnet-beta.solana.com -X POST -H "Content-Type:
 
 # MSOL/USDC
 # https://explorer.solana.com/tx/34ZbBUv1xs8QXBfDVDTUsqQEvRxy4goRtcct1w7SLzmFPkRhF6SCa2ptzDGBgUwZ67uDxxsQER7qWo3upJa2JY3b
-# crank_grpf = `curl https://lb-ams2.rpcpool.com/20dc90af2aed522cdff09bab309e -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getRecentPrioritizationFees", "params": [["BLcmYWJXZzM5ugabv8t9MGt5uZeQpLeeVHJEucb6JoM1", "9Lyhks5bQQxb9EyyX55NtgKQzpM4WK7JCmeaWuQ5MoXD", "B79QctKrkmGePVZp22TgPZfWdVAiXKwYqzREh82G9MfW","Bzmr3xA8sWsHogARhFvJALR9o58xXk4jUnPZ4ARhez8w", "CsB4XjwH4uZRjRXEXRBFJ3hi3mb9jujzRCW7NXg7TRtX"]]}'`
+# crank_grpf = `curl https://api.mainnet-beta.solana.com -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getRecentPrioritizationFees", "params": [["BLcmYWJXZzM5ugabv8t9MGt5uZeQpLeeVHJEucb6JoM1", "9Lyhks5bQQxb9EyyX55NtgKQzpM4WK7JCmeaWuQ5MoXD", "B79QctKrkmGePVZp22TgPZfWdVAiXKwYqzREh82G9MfW","Bzmr3xA8sWsHogARhFvJALR9o58xXk4jUnPZ4ARhez8w", "CsB4XjwH4uZRjRXEXRBFJ3hi3mb9jujzRCW7NXg7TRtX"]]}'`
 
 # https://explorer.solana.com/tx/2uUBHLaMo8ztCoQM918bd87EbmsTEadgNoP63LAUNjBvZfTxQB1ccNmesYdfiHcknGDFfHqYvYv6tjo1MQLXRfG7
-# crank_grpf = `curl https://nyc50.rpcpool.com/20dc90af2aed522cdff09bab309e -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getRecentPrioritizationFees", "params": [["8cMWuNmjfPFs2R2m3A7uzM4CPTFA5d2savEUeSRBGQ9x", "GBtLdkr21znHJDhU4vXmsNcE3wfQB8nd5eQErMLyDT93", "2PyDB2mgEgT7WnC6K9p7jWvELcU9xB7VzLsrcddG4FVs","CaDAUgEtYu5v28B6jp2RUp5kDQdiCZrdEcQN77nCxECV", "Dj6Vt2Dar6EivimUSZKRayuLpyAefDaGaYdecWzeuY6k","ADaXG254WcLm63J94mRLvLevu4GM1A4oyyYkyArEYmrG", "AtK4PHqmqXJGZdbsW3ztyZXzBt9VDaRi3e1Fj4ZFksJQ", "7Rys2xVfRfdB1Kxc7N3t4cy1PF4FnQHKEzav25Trp4EN", "3SmUefUMEuhy5eqa9y5PCkpapuyThX2ntphX8ZQ97vqt", "7yQbisr56CZHvHmTZCvtAFYJjBBXrAFSBGvzRwny4BWm", "ENFC7QDMoJeQV84ma12Vi8VzDUwnYBi31f9rHkRx3TEX", "4MNgw4apQ8kCQ2AvL7CM9WBQAqatt71xAmZKW8qSPGcP",  "BKcNWr11FPrgaRbGnE766Zb2u3ygcXG97re6DR99SHnq", "2VPaLbSd7mGhPChZX8fHLhXaVADRp8HHfkzDAmh7DM3m", "9aSYbBen5a8CUEV3nkC8KDoM8F5bpcnqytSrEGVwqZT5", "FbjLh8Qgsg5RPBpnbqX6RT3dJTwL9v2f1rSfLbGTSbn8", "6482e33zrerYfhKAjPR2ncMSrH2tbTy5LDjdhB5PXzxd", "3BQCyicEHqXVsQicA8xeoTLHjMD5HPR2BiCZsPyJTvdg", "AtWMMJa1dMSfWHWVGbpuSuk4a645kayfb4vrFVjeBHif", "3uP3wUyVHGPzhT7gkDsQWMc3aqH1S8i9Vv3kdAiDt6i3", "Afw4B8B9arBkWGcr8C9UvgrGjxc35Y5afDA7MTZrPKAW"]]}'`
+# crank_grpf = `curl https://api.mainnet-beta.solana.com -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getRecentPrioritizationFees", "params": [["8cMWuNmjfPFs2R2m3A7uzM4CPTFA5d2savEUeSRBGQ9x", "GBtLdkr21znHJDhU4vXmsNcE3wfQB8nd5eQErMLyDT93", "2PyDB2mgEgT7WnC6K9p7jWvELcU9xB7VzLsrcddG4FVs","CaDAUgEtYu5v28B6jp2RUp5kDQdiCZrdEcQN77nCxECV", "Dj6Vt2Dar6EivimUSZKRayuLpyAefDaGaYdecWzeuY6k","ADaXG254WcLm63J94mRLvLevu4GM1A4oyyYkyArEYmrG", "AtK4PHqmqXJGZdbsW3ztyZXzBt9VDaRi3e1Fj4ZFksJQ", "7Rys2xVfRfdB1Kxc7N3t4cy1PF4FnQHKEzav25Trp4EN", "3SmUefUMEuhy5eqa9y5PCkpapuyThX2ntphX8ZQ97vqt", "7yQbisr56CZHvHmTZCvtAFYJjBBXrAFSBGvzRwny4BWm", "ENFC7QDMoJeQV84ma12Vi8VzDUwnYBi31f9rHkRx3TEX", "4MNgw4apQ8kCQ2AvL7CM9WBQAqatt71xAmZKW8qSPGcP",  "BKcNWr11FPrgaRbGnE766Zb2u3ygcXG97re6DR99SHnq", "2VPaLbSd7mGhPChZX8fHLhXaVADRp8HHfkzDAmh7DM3m", "9aSYbBen5a8CUEV3nkC8KDoM8F5bpcnqytSrEGVwqZT5", "FbjLh8Qgsg5RPBpnbqX6RT3dJTwL9v2f1rSfLbGTSbn8", "6482e33zrerYfhKAjPR2ncMSrH2tbTy5LDjdhB5PXzxd", "3BQCyicEHqXVsQicA8xeoTLHjMD5HPR2BiCZsPyJTvdg", "AtWMMJa1dMSfWHWVGbpuSuk4a645kayfb4vrFVjeBHif", "3uP3wUyVHGPzhT7gkDsQWMc3aqH1S8i9Vv3kdAiDt6i3", "Afw4B8B9arBkWGcr8C9UvgrGjxc35Y5afDA7MTZrPKAW"]]}'`
 
 # No params to show minimum fee to get into the block
 # crank_grpf = `curl https://api.mainnet-beta.solana.com -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getRecentPrioritizationFees"}'`
@@ -48,7 +48,7 @@ end
 
 # puts ''
 
-# all_grpf = `curl https://lb-ams2.rpcpool.com/20dc90af2aed522cdff09bab309e -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getRecentPrioritizationFees"}'`
+# all_grpf = `curl https://api.mainnet-beta.solana.com -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getRecentPrioritizationFees"}'`
 # all_grpf_parsed = JSON.parse(all_grpf)
 # puts all_grpf_parsed
 
