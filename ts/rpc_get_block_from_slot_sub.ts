9426f15dc10a0345f46726a80858353b2fa153e0
Wilfred Almeida <almeidawilfred642@gmail.com>
2025-01-21 20:24:49 +0530

    feat: script ts/rpc_get_block_from_slot_sub.ts
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

---
+++ ts/rpc_get_block_from_slot_sub.ts
@@ -0,0 +1,35 @@
+import { Connection } from "@solana/web3.js"
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const RPC_ENDPOINT = process.env.RPC_ENDPOINT || "https://api.mainnet-beta.solana.com"
+
+const COMMITMENT_LEVEL = "finalized"
+
+console.log(`Using commitment level ${COMMITMENT_LEVEL}`);
+
+
+async function main() {
+  const connection = new Connection(RPC_ENDPOINT)
+
+  while (true) {
+    const slot = await connection.getSlot({ commitment: COMMITMENT_LEVEL })
+    console.log(`${new Date().toUTCString()} Got Slot: ${slot}`)
+
+    while (true) {
+      const startTime = Date.now()
+      let tries = 1;
+      try {
+        const block = await connection.getBlock(slot, { commitment: COMMITMENT_LEVEL, rewards: true, transactionDetails: "full", maxSupportedTransactionVersion: 0 })
+        if (block != null) {
+          console.log(`${new Date().toUTCString()} Got Block ${slot} in ${Date.now() - startTime}ms`);
+          break;
+        }
+      } catch (e: any) { }
+    }
+
+  }
+
+}
+main()
